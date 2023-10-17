c6c1f9ea24f5fb8e183698dd21176a850d2404a4
JackPreece <jack.preece@sero.life>
2023-10-17 09:44:37 +0100

    rename elements

--- wind_vector.py
+++ wind_vector.py
@@ -2,19 +2,19 @@ import streamlit as st
 import numpy as np
 
 
-def compass_vector_addition(vector1, vector2):
-    angle1, magnitude1 = vector1
-    angle2, magnitude2 = vector2
+def compass_vector_addition(apparent_wind, boat_velocity):
+    wind_angle, wind_speed = apparent_wind
+    course_bearing, boat_speed = boat_velocity
 
     # Convert compass bearing and magnitude to Cartesian coordinates (x, y)
-    angle1_rad = np.radians(angle1)
-    angle2_rad = np.radians(angle2)
+    wind_angle_rad = np.radians(wind_angle)
+    course_bearing_rad = np.radians(course_bearing)
 
-    x1 = magnitude1 * np.cos(angle1_rad)
-    y1 = magnitude1 * np.sin(angle1_rad)
+    x1 = wind_speed * np.cos(wind_angle_rad)
+    y1 = wind_speed * np.sin(wind_angle_rad)
 
-    x2 = magnitude2 * np.cos(angle2_rad)
-    y2 = magnitude2 * np.sin(angle2_rad)
+    x2 = boat_speed * np.cos(course_bearing_rad)
+    y2 = boat_speed * np.sin(course_bearing_rad)
 
     # Perform vector addition in Cartesian coordinates
     result_x = x1 + x2
@@ -26,7 +26,7 @@ def compass_vector_addition(vector1, vector2):
     result_angle_deg = np.degrees(result_angle_rad)
 
     result_angle_deg = round((result_angle_deg + 360) % 360, 0)
-    wind_angle_deg = result_angle_deg - angle2
+    wind_angle_deg = result_angle_deg - course_bearing
     if wind_angle_deg < 0:
         wind_angle_deg += 360
 
@@ -43,17 +43,17 @@ def compass_vector_addition(vector1, vector2):
 
 st.title("Sail selection tool")
 st.write("Enter the details of wind and course:")
-vector1_angle = st.number_input("Apparent Wind Direction - due to tide(degrees):", min_value=0, max_value=360, value=0)
-vector1_magnitude = st.number_input("Apparent Wind Speed (knots):", min_value=0, value=10)
-vector2_angle = st.number_input("Bearing to next mark (degrees):", min_value=0, max_value=360, value=0)
-vector2_magnitude = st.number_input("Estimated Boat Speed SOG(knots):", min_value=0, value=5)
+apparent_wind_angle = st.number_input("Apparent Wind Direction - due to tide(degrees):", min_value=0, max_value=360, value=0)
+apparent_wind_magnitude = st.number_input("Apparent Wind Speed (knots):", min_value=0, value=10)
+boat_velocity_angle = st.number_input("Bearing to next mark (degrees):", min_value=0, max_value=360, value=0)
+boat_velocity_magnitude = st.number_input("Estimated Boat Speed SOG(knots):", min_value=0, value=5)
 
 if st.button("Select my sail"):
     st.header("Results")
-    vector1 = (vector1_angle, vector1_magnitude)
-    vector2 = (vector2_angle, vector2_magnitude)
+    apparent_wind = (apparent_wind_angle, apparent_wind_magnitude)
+    boat_velocity = (boat_velocity_angle, boat_velocity_magnitude)
 
-    result_angle, result_magnitude, wind_angle_deg, sail = compass_vector_addition(vector1, vector2)
+    result_angle, result_magnitude, wind_angle_deg, sail = compass_vector_addition(apparent_wind, boat_velocity)
 
     st.write(f"Resultant Wind from tide and boat speed: {result_angle} degrees, {result_magnitude} knots.")
     st.write(f" Wind angle to Course Over Ground: {wind_angle_deg} degrees.")
